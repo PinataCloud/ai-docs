b196626619d89ce9272c44c524b38f9ddd23e5ba
Steve <stevedsimkins@gmail.com>
2024-11-01 16:47:31 -0400

    switch to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+	output: "static",
+	adapter: vercel(),
 });
