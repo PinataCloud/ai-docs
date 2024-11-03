beea0f0095dc9d1a360e2bf4b957c293f6d9befa
Steve <stevedsimkins@gmail.com>
2024-11-03 15:34:53 -0500

    Added IPFS Docs

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -1,6 +1,14 @@
-export async function getDocContent() {
+export async function getFilesContent() {
 	const request = await fetch(
 		"https://raw.githubusercontent.com/PinataCloud/pinata/refs/heads/main/ai-instructions",
+	);
+	const content = await request.text();
+	return content;
+}
+
+export async function getIPFSContent() {
+	const request = await fetch(
+		"https://raw.githubusercontent.com/PinataCloud/pinata-web3/refs/heads/main/ai-instructions",
 	);
 	const content = await request.text();
 	return content;
